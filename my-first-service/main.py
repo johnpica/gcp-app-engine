1a01328d0bcf38427991b843fe2cae570a679b8c
John Pica <john.pica@gmail.com>
2022-02-24 07:13:27 +0000

    first service

--- my-first-service/main.py
+++ my-first-service/main.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'My First Service - V1'
+def main():
+    return 'First Service! v1'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
